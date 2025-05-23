9cd71b6fd570f9999e7ccd77ddca80d2927d3404
Николай Немов <prost91@outlook.com>
2025-05-23 20:17:11 +0300

    Functions separation DONE

---
+++ Functions/Arrays/Sort.h
@@ -0,0 +1,10 @@
+#pragma once
+
+void Sort(int arr[], const int n);
+void Sort(double arr[], const int n);
+
+
+
+
+
+
--- Functions/Arrays/main.cpp
+++ Functions/Arrays/main.cpp
@@ -1,25 +1,24 @@
-#include<iostream>
-using namespace std;
+#include"stdafx.h"
+#include"constants.h"
+#include"FillRand.h"
+#include"Print.h"
+#include"Sort.h"
+#include"Statistics.h"
+#include"Shift.h"
 
-#define tab "\t"
+// Header-file
 
-void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100);
-void FillRand(double arr[], const int n, int minRand = 0, int maxRand = 100);
 
-void Print(int arr[], const int n);
-void Print(double arr[], const int n);
 
-void Sort(int arr[], const int n);
-void Sort(double arr[], const int n);
 
-int Sum(int arr[], const int n);
-double Avg(int arr[], const int n);
 
-int minValueIn(int arr[], const int n);
-int maxValueIn(int arr[], const int n);
 
-void ShiftLeft(int arr[], const int n, const int number_of_shifts);
-void ShiftRight(int arr[], const int n, const int number_of_shifts);
+
+
+
+
+
+
 
 void main()
 {
@@ -49,115 +48,8 @@ void main()
 	//Print(brr, SIZE);
 }
 
-void FillRand(int arr[], const int n, int minRand, int maxRand)
-{
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = rand() % (maxRand - minRand) + minRand;
-	}
-}
-void FillRand(double arr[], const int n, int minRand, int maxRand)
-{
-	minRand *= 100;
-	maxRand *= 100;
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = rand() % (maxRand - minRand) + minRand;
-		arr[i] /= 100;
-	}
-}
 
-void Print(int arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << tab;
-	}
-	cout << endl;
-}
-void Print(double arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		cout << arr[i] << tab;
-	}
-	cout << endl;
-}
 
-void Sort(int arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = i + 1; j < n; j++)
-		{
-			if (arr[j] < arr[i])
-			{
-				int buffer = arr[i];
-				arr[i] = arr[j];
-				arr[j] = buffer;
-			}
-		}
-	}
-}
-void Sort(double arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = i + 1; j < n; j++)
-		{
-			if (arr[j] < arr[i])
-			{
-				double buffer = arr[i];
-				arr[i] = arr[j];
-				arr[j] = buffer;
-			}
-		}
-	}
-}
-int Sum(int arr[], const int n)
-{
-	int sum = 0;
-	for (int i = 0; i < n; i++)
-	{
-		sum += arr[i];
-	}
-	return sum;
-}
-double Avg(int arr[], const int n)
-{
-	return (double)Sum(arr, n) / n;
-}
-int minValueIn(int arr[], const int n)
-{
-	int min = arr[0];
-	for (int i = 0; i < n; i++)
-	{
-		if (arr[i] < min)min = arr[i];
-	}
-	return min;
-}
-int maxValueIn(int arr[], const int n)
-{
-	int max = arr[0];
-	for (int i = 0; i < n; i++)
-	{
-		if (arr[i] > max)max = arr[i];
-	}
-	return max;
-}
-void ShiftLeft(int arr[], const int n, const int number_of_shifts)
-{
-	for (int i = 0; i < number_of_shifts; i++)
-	{
-		int buffer = arr[0];
-		for (int i = 1; i < n; i++)
-		{
-			arr[i - 1] = arr[i];
-		}
-		arr[n - 1] = buffer;
-	}
-}
-void ShiftRight(int arr[], const int n, const int number_of_shifts)
-{
-	ShiftLeft(arr, n, n - number_of_shifts);
-}
+
+
+
